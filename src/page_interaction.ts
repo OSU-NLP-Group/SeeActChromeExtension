c1d51f1a377608465b638cdfef3a1c6934e51b6d
BareBeaverBat <ssiliss72@gmail.com>
2024-03-29 11:54:56 -0400

    confirm stage 2 demo fully working
    obv not committing the oai api key
    
    Signed-off-by: BareBeaverBat <ssiliss72@gmail.com>

--- src/page_interaction.ts
+++ src/page_interaction.ts
@@ -2,6 +2,10 @@ import {BrowserHelper} from "./utils/BrowserHelper";
 import {formatChoices, generatePrompt, postProcessActionLlm, StrTriple} from "./utils/format_prompts";
 import {OpenAiEngine} from "./utils/OpenAiEngine";
 import {getIndexFromOptionName} from "./utils/format_prompt_utils";
+import {sleep} from "openai/core";
+
+//todo explore winston or some other proper logging library so content script's logging doesn't get lost
+// as soon as a navigation step happens
 
 console.log("successfully injected page_interaction script in browser");
 
@@ -9,7 +13,7 @@ const browserHelper = new BrowserHelper();
 const currInteractiveElements = browserHelper.getInteractiveElements();
 
 const interactiveChoiceDetails = currInteractiveElements.map<StrTriple>((element) => {
-    return [element.tagHead, element.description, element.tagName];
+    return [element.description, element.tagHead, element.tagName];
 });
 
 const candidateIds = currInteractiveElements.map((element, index) => {
@@ -23,6 +27,8 @@ const candidateIds = currInteractiveElements.map((element, index) => {
 const interactiveChoices = formatChoices(interactiveChoiceDetails, candidateIds);
 
 const prompts = generatePrompt("Pick a random interactive element in the current page which seems even a bit interesting and click on it", [], interactiveChoices);
+
+console.log("prompts:", prompts);
 
 const modelName: string = "gpt-4-vision-preview";
 const apiKey: string = "PLACEHOLDER";
@@ -38,21 +44,25 @@ const aiEngine = new OpenAiEngine(modelName, apiKey);
     console.assert(screenshotDataUrl !== undefined, "screenshot data url is undefined")
     console.log("screenshot data url (truncated):", screenshotDataUrl.slice(0, 100));
 
-    const planningOutput = await aiEngine.generate(prompts, 0, screenshotDataUrl);
+    const planningOutput = await aiEngine.generateWithRetry(prompts, 0, screenshotDataUrl);
 
     console.log("planning output:", planningOutput);
 
-    const groundingOutput = await aiEngine.generate(prompts, 1, screenshotDataUrl, planningOutput);
+    const groundingOutput = await aiEngine.generateWithRetry(prompts, 1, screenshotDataUrl, planningOutput);
 
     console.log("grounding output:", groundingOutput);
 
     const [elementName, actionName, value] = postProcessActionLlm(groundingOutput);
     console.log("suggested action:", actionName, "; value:", value);
     const chosenElementIndex = getIndexFromOptionName(elementName);
-    const chosenElement = currInteractiveElements[chosenElementIndex];
+    console.log("clicking on the", chosenElementIndex, "entry from the candidates list; which is the", candidateIds[chosenElementIndex], "element of the original interactiveElements list");
+    const chosenElement = currInteractiveElements[candidateIds[chosenElementIndex]];
     const elementToClick = chosenElement.element;
 
     console.log("element to click:", chosenElement.tagHead, "; description:", chosenElement.description);
+
+
+    await sleep(5000);//to allow copying the logging from chrome dev console before the click happens and the new page loads
 
     elementToClick.click();
 
