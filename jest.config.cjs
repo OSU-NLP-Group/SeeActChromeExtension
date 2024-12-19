d37d9537f54fb7310a36f23673820907c9964068
BareBeaverBat <salisbury.100@buckeyemail.osu.edu>
2024-12-19 10:59:51 -0500

    get jest configs into subfolder
    Signed-off-by: BareBeaverBat <salisbury.100@buckeyemail.osu.edu>

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    setupFiles: ['<rootDir>/setupTests.cjs'],
-    resetMocks: true,
-    randomize: true,
-    testMatch: ['<rootDir>/tests/**/*.test.ts'],
-};
